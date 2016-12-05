b59485fd06a8ef822b916c7218e770dad002cca1
Boglarka <bogi.11@hotmail.com>
2016-12-05 22:19:31 +0100

    [2] modified gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -2,20 +2,16 @@
 
 var gulp = require('gulp');
 var sass = require('gulp-sass');
-var server = require('gulp-express');
 
 gulp.task('sass', function () {
-    return gulp.src('./app/style/**/*.scss')
+    return gulp.src('./app/style/*.css')
         .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest('./app/style'));
 });
 
 gulp.task('sass:watch', function () {
-    gulp.watch('./app/style/**/*.scss', ['sass']);
+    gulp.watch('./app/scss/*.scss', ['sass']);
 });
 
-gulp.task('server', function() {
-    server.run(['./app.js']);
-});
 
-gulp.task('default', ['sass:watch', 'server']);
+gulp.task('default', ['sass:watch']);

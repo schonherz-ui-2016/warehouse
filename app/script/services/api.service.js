8e2a18f74d5668e36fa261dcfe96616c57778b6a
Magyar Gábor <hungaro.gabor@gmail.com>
2016-12-08 12:06:36 +0100

    #8 Update api.service.js

--- app/script/services/api.service.js
+++ app/script/services/api.service.js
@@ -1,3 +1,14 @@
 /**
  * Created on 2016.12.08..
  */
+
+(function () {
+    angular
+        .module('warehouse')
+        .service('api', Service);
+
+    function Service($http){
+        
+    }
+
+})();

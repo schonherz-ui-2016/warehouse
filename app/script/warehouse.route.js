8544a9c2cbaa4a00043603ab43d93c3fb16b4f6e
Boglarka <bogi.11@hotmail.com>
2016-12-18 19:50:08 +0100

    #27 Add login and registration to route.js

--- app/script/warehouse.route.js
+++ app/script/warehouse.route.js
@@ -9,6 +9,15 @@
                     controller: 'main',
                     controllerAs: 'main'
                 })
+                .when('/login',{
+                    templateUrl: 'pages/login/login.html',
+                    controller: 'loginController'
+                })
+                .when('/register',{
+                    templateUrl: 'pages/registration/registration.html',
+                    controller: 'registrationController'
+                })
+
                 .otherwise({
                     templateUrl: 'pages/404/404.html'
                 });

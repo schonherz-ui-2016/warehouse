8a1e4d0f036b2a92c6f73f2444190c3313420767
Gaben9109 <hungaro.gabor@gmail.com>
2016-12-23 10:54:09 +0100

    #30 Failed login

--- app/pages/login/login.controller.js
+++ app/pages/login/login.controller.js
@@ -6,11 +6,15 @@
     angular.module('warehouse')
         .controller('login', function ($scope, api, $location) {
             var vm = this;
+            vm.alert = false;
             vm.login = function () {
                 api.login(vm.email, vm.password)
                     .then(function () {
                         $location.path('/warehouse');
-                    });
+                    })
+                    .catch(function () {
+                        vm.alert = true;
+                });
             }
-        })
+        });
 })();

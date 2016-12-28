8fbdff6c6e115c95c263e3be359c719e73108c61
Gaben9109 <hungaro.gabor@gmail.com>
2016-12-28 13:24:12 +0100

    #29 Password validation

--- app/pages/registration/registration.controller.js
+++ app/pages/registration/registration.controller.js
@@ -6,15 +6,37 @@
         .module('warehouse')
         .controller('registration', function ($scope, api, $location) {
             var vm = this;
+
+            vm.isInvalid = false;
+            vm.isPasswordsNotEqual = false;
+            vm.error = false;
+            vm.passwordPattern = "^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{8,}$";
+
             vm.registration = function () {
-                api.registration({
-                    "email": vm.email,
-                    "password": vm.password
-                })
-                    .then(function (result) {
-                        console.log(result.config.data);
-                        $location.path('/');
-                    });
+                vm.isInvalid = false;
+                vm.isPasswordsNotEqual = false;
+                vm.error = false;
+
+                if (vm.password == undefined) {
+                    vm.isInvalid = true;
+                }
+                else if (vm.password !== vm.confirmPassword) {
+                    vm.isPasswordsNotEqual = true;
+                }
+                else {
+                    api.registration({
+                        "email": vm.email,
+                        "password": vm.password
+                    })
+                        .then(function (result) {
+                            console.log(result.config.data);
+                            $location.path('/');
+                        })
+                        .catch(function (error) {
+                            console.log(error);
+                            vm.error = true;
+                        });
+                }
             }
         });
 })();
